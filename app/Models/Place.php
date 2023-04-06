e2568470f9a067ef0f4aedff4bb11ab4bd708b3e
Arwa <arwanayef2001@hotmail.com>
2023-04-06 14:38:45 +0300

    Deleting weightRating method from the model

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -9,9 +9,4 @@ class Place extends Model
 {
     use HasFactory;
 
-    public function weightedRating($weightFactor , $minUsers)
-    {
-        $weightedRating = ($weightFactor * $this->avg_rating) + ((1 - $weightFactor) * ($this->user_number / ($this->user_number + $minUsers)));
-        return $weightedRating;
-    }
 }
